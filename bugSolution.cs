369d0c7fe91989b707c5137adb60818f012df876
BugHunter-X <askzenai@gmail.com>
2024-12-25 21:50:05 +0100

    Add bugSolution.cs file

---
+++ bugSolution.cs
@@ -0,0 +1,10 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public void MyMethod()
+    {
+        // Accessing the existing property
+        Console.WriteLine(this.MyProperty); 
+    }
+}
